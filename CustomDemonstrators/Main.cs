311ee8e38fd9a71da47e555013bc086172583284
Isaac M. Good <imgoodproductions@sbcglobal.net>
2026-06-12 11:40:53 -0700

    Scaffold project based on DV modding template

---
+++ CustomDemonstrators/Main.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Reflection;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace CustomDemonstrators;
+
+public static class Main
+{
+    private static bool Load(UnityModManager.ModEntry modEntry)
+    {
+        Harmony? harmony = null;
+
+		try
+		{
+			harmony = new Harmony(modEntry.Info.Id);
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+			// Other plugin startup logic
+		}
+		catch (Exception ex)
+		{
+			modEntry.Logger.LogException($"Failed to load {modEntry.Info.DisplayName}:", ex);
+			harmony?.UnpatchAll(modEntry.Info.Id);
+			return false;
+		}
+
+		return true;
+    }
+}
